aad0469dd074798b3a6aef17773bbeaa4f0dcc52
Jonatas Alves <jonatasalves@id.uff.br>
2021-07-31 08:51:34 -0300

    correções

--- main.c
+++ main.c
@@ -2,133 +2,53 @@
 
 int main()
 {
-  // CORREÇÕES nºos aleatórios e sequências:
-  //n = quantidade de dados na seuqência
-  // primeiro, terão 5 sequências de n valores (ex. 10)
-  // depois, terão 5 sequências de outro n valores (ex. 50, 100, 150, 200, 250...)
-  // o intervalo dos números será: [1, MAX] = intervalo onde serão gerados os numeros aleatórios
-  // ex: MAX = 1.000.000
-
-  // TODO:
-  // - Ajustar a geração de sequências e nºs aleatórios, conforme as correções acima;
-  // - Entregar o notion + gráficos com a análise dos algoritmos;
-  // - Colocar explicação verbal da Complexidade;
-  // - Vídeo explicando o trabalho;
-  // - Enviar até DOMINGO o .zip com o README com a orientação de como rodar o código + o código + o vídeo + a análise (em pdf?)
-
-  double s_CPU_final_1;
-  double s_CPU_inicial_1;
-  double s_total_inicial_1;
-  double s_total_final_1;
-  double s_CPU_final_2;
-  double s_CPU_inicial_2;
-  double s_total_inicial_2;
-  double s_total_final_2;
-  double s_CPU_final_3;
-  double s_CPU_inicial_3;
-  double s_total_inicial_3;
-  double s_total_final_3;
-  double s_CPU_final_4;
-  double s_CPU_inicial_4;
-  double s_total_inicial_4;
-  double s_total_final_4;
-  double s_CPU_final_5;
-  double s_CPU_inicial_5;
-  double s_total_inicial_5;
-  double s_total_final_5;
   int i,
+      N = 100,
       valorMax = 10000,
-      vet1[valorMax],
-      vet2[valorMax * 2],
-      vet3[valorMax * 3],
-      vet4[valorMax * 4],
-      vet5[valorMax * 5];
-  for (i = 0; i < valorMax; i++)
+      vet1[N],
+      vet2[N],
+      vet3[N],
+      vet4[N],
+      vet5[N],
+      media;
+
+  double seg_CPU_total_1, seg_sistema_total_1,
+      seg_CPU_total_2, seg_sistema_total_2,
+      seg_CPU_total_3, seg_sistema_total_3,
+      seg_CPU_total_4, seg_sistema_total_4,
+      seg_CPU_total_5, seg_sistema_total_5;
+
+  for (i = 0; i < N; i++)
   {
     vet1[i] = gerarNumero(valorMax);
+    vet2[i] = gerarNumero(valorMax);
+    vet3[i] = gerarNumero(valorMax);
+    vet4[i] = gerarNumero(valorMax);
+    vet5[i] = gerarNumero(valorMax);
   }
 
-  for (i = 0; i < valorMax * 2; i++)
-  {
-    vet2[i] = gerarNumero(valorMax * 2);
-  }
+  Tempo_CPU_Sistema(&seg_CPU_total_1, &seg_sistema_total_1);
+  mergesort(vet1, valorMax);
+  Tempo_CPU_Sistema(&seg_CPU_total_1, &seg_sistema_total_1);
 
-  for (i = 0; i < valorMax * 3; i++)
-  {
-    vet3[i] = gerarNumero(valorMax * 3);
-  }
+  Tempo_CPU_Sistema(&seg_CPU_total_2, &seg_sistema_total_2);
+  mergesort(vet2, valorMax);
+  Tempo_CPU_Sistema(&seg_CPU_total_2, &seg_sistema_total_2);
 
-  for (i = 0; i < valorMax * 4; i++)
-  {
-    vet4[i] = gerarNumero(valorMax * 4);
-  }
+  Tempo_CPU_Sistema(&seg_CPU_total_3, &seg_sistema_total_3);
+  mergesort(vet3, valorMax);
+  Tempo_CPU_Sistema(&seg_CPU_total_3, &seg_sistema_total_3);
 
-  for (i = 0; i < valorMax * 5; i++)
-  {
-    vet5[i] = gerarNumero(valorMax * 5);
-  }
+  Tempo_CPU_Sistema(&seg_CPU_total_4, &seg_sistema_total_4);
+  mergesort(vet4, valorMax);
+  Tempo_CPU_Sistema(&seg_CPU_total_4, &seg_sistema_total_4);
 
-  Tempo_CPU_Sistema(&s_CPU_inicial_1, &s_total_inicial_1);
-  quicksort(vet1, valorMax);
-  Tempo_CPU_Sistema(&s_CPU_final_1, &s_total_final_1);
-  printf ("Tempo de CPU total 1 = %f\n", s_CPU_final_1 - s_CPU_inicial_1);
+  Tempo_CPU_Sistema(&seg_CPU_total_5, &seg_sistema_total_5);
+  mergesort(vet5, valorMax);
+  Tempo_CPU_Sistema(&seg_CPU_total_5, &seg_sistema_total_5);
 
-  Tempo_CPU_Sistema(&s_CPU_inicial_2, &s_total_inicial_2);
-  quicksort(vet2, valorMax * 2);
-  Tempo_CPU_Sistema(&s_CPU_final_2, &s_total_final_2);
-  printf ("Tempo de CPU total 2 = %f\n", s_CPU_final_2 - s_CPU_inicial_2);
-  
-  Tempo_CPU_Sistema(&s_CPU_inicial_3, &s_total_inicial_3);
-  quicksort(vet3, valorMax * 3);
-  Tempo_CPU_Sistema(&s_CPU_final_3, &s_total_final_3);
-  printf ("Tempo de CPU total 3 = %f\n", s_CPU_final_3 - s_CPU_inicial_3);
-
-  Tempo_CPU_Sistema(&s_CPU_inicial_4, &s_total_inicial_4);
-  quicksort(vet4, valorMax * 4);
-  Tempo_CPU_Sistema(&s_CPU_final_4, &s_total_final_4);
-  printf ("Tempo de CPU total 4 = %f\n", s_CPU_final_4 - s_CPU_inicial_4);
-
-  Tempo_CPU_Sistema(&s_CPU_inicial_5, &s_total_inicial_5);
-  quicksort(vet5, valorMax * 5);
-  Tempo_CPU_Sistema(&s_CPU_final_5, &s_total_final_5);
-  printf ("Tempo de CPU total 5 = %f\n", s_CPU_final_5 - s_CPU_inicial_5);
-
-  // for (int j = 0; j < valorMax; j++)
-  // {
-  //   printf("%d ", vet1[j]);
-  // }
-
-  // printf("\n");
-
-  // for (int k = 0; k < valorMax * 2; k++)
-  // {
-  //   printf("%d ", vet2[k]);
-  // }
-
-  // printf("\n");
-
-  // for (int k = 0; k < valorMax * 3; k++)
-  // {
-  //   printf("%d ", vet3[k]);
-  // }
-
-  // printf("\n");
-
-  // for (int k = 0; k < valorMax * 4; k++)
-  // {
-  //   printf("%d ", vet4[k]);
-  // }
-
-  // printf("\n");
-
-  // for (int k = 0; k < valorMax * 5; k++)
-  // {
-  //   printf("%d ", vet5[k]);
-  // }
-
-  // printf("\n");
-
-  // printf ("Custo = %d\n", custo);
+  media += (seg_CPU_total_1 + seg_CPU_total_2 + seg_CPU_total_3 + seg_CPU_total_4 + seg_CPU_total_5) / N;
+  printf("Média: %f", media);
 
   return 0;
 }
